3d5f46bd3a3ada015f53c320317aed51dc7522cd
Phill Okema <philokema7@gmail.com>
2025-06-18 12:07:36 +0300

    Name change

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,10 +1,18 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 
 const search = () => {
   return (
-    <View>
-      <Text>search</Text>
+    <View className='flex-1 items-center justify-center bg-dark-100'>
+      <Text className='font-bold'>Search</Text>
+      <Image
+        source={require('@/assets/images/rankingGradient.png')}
+        className='w-24 h-24'
+        resizeMode='contain'/>
+        <TouchableOpacity
+          className='bg-primary-500 px-4 py-2 rounded-lg mt-4'
+          onPress={() => alert('Search functionality coming soon!')}></TouchableOpacity>
+          <Text className='text-white font-bold'>Search Movies</Text>
     </View>
   )
 }
